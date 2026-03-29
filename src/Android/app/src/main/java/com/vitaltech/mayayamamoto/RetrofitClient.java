f0f8723fd58fa47493f068555965fe8f39960c36
Murilo Ângelo <murapb07@gmail.com>
2026-03-29 14:59:16 -0300

    criacao do apk

--- src/Android/app/src/main/java/com/vitaltech/mayayamamoto/RetrofitClient.java
+++ src/Android/app/src/main/java/com/vitaltech/mayayamamoto/RetrofitClient.java
@@ -6,7 +6,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     // Endereco do servidor
+
+    // IP padrao para utilizar no EMULADOR do Android Studio
     private static final String BASE_URL = "http://10.0.2.2:5000/";
+
+    // IP do computador
+    //private static final String BASE_URL = "http://xxx.xxx.x.x:5000/";
+
     // Instancia unica do Retrofit
     private static Retrofit retrofit;
 
